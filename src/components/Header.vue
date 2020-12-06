035cbcba4876d77fcbe8d1950832e71a17ebc9e3
rusakov <dan-rusakov@mail.ru>
2020-12-07 01:26:30 +0500

    adaptive header and footer

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -2,22 +2,24 @@
   <header class="header">
     <nav class="header__wrapper">
       <div class="header__content-box">
-        <div class="header__top-line">
-          <a :href="`tel:${formattedPhone}`" class="header__phone">{{ phone }}</a>
-        </div>
-        <div class="header__bottom-line">
-          <ul class="header__menu">
-            <li class="header__menu-item">
-              <router-link :to="{name: 'Home', hash: '#about'}" @click.native="scrollFix('#about')" class="header__menu-link" tag="a">О компании</router-link>
-            </li>
-            <li class="header__menu-item">
-              <router-link :to="{name: 'Products'}" class="header__menu-link" tag="a">Продукция</router-link>
-            </li>
-            <li class="header__menu-item">
-              <router-link :to="{name: 'Logistics'}" class="header__menu-link" tag="a">Логистика</router-link>
-            </li>
-          </ul>
-        </div>
+        <a :href="`tel:${formattedPhone}`" class="header__phone">{{ phone }}</a>
+        <ul class="header__menu">
+          <li class="header__menu-item">
+            <router-link
+              :to="{name: 'Home', hash: '#about'}"
+              @click.native="scrollFix('#about')"
+              class="header__menu-link"
+              tag="a"
+            >О компании</router-link>
+          </li>
+          <li class="header__menu-item">
+            <router-link :to="{name: 'Products'}" class="header__menu-link" tag="a">Продукция</router-link>
+          </li>
+          <li class="header__menu-item">
+            <router-link :to="{name: 'Logistics'}" class="header__menu-link" tag="a">Логистика</router-link>
+          </li>
+        </ul>
+        <button type="button" class="header__popup-btn header__popup-btn--tablet" @click="openCallbackPopup = true">Заказать звонок</button>
       </div>
       <div class="header__content-box">
         <router-link :to="{name: 'Home'}" class="header__logo-box" tag="a">
@@ -25,27 +27,83 @@
         </router-link>
       </div>
       <div class="header__content-box">
-        <div class="header__content-box">
-          <div class="header__top-line">
-            <button type="button" class="header__popup-btn" @click="openCallbackPopup = true">Заказать звонок</button>
-          </div>
-          <div class="header__bottom-line">
-            <ul class="header__menu">
-              <li class="header__menu-item">
-                <router-link :to="{name: 'PriceList'}" class="header__menu-link" tag="a">Прайс-лист</router-link>
-              </li>
-              <li class="header__menu-item">
-                <router-link :to="{name: 'Dealers'}" class="header__menu-link" tag="a">Поставщикам</router-link>
-              </li>
-              <li class="header__menu-item">
-                <router-link :to="{name: 'Contacts'}" class="header__menu-link" tag="a">Контакты</router-link>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <button type="button" class="header__popup-btn" @click="openCallbackPopup = true">Заказать звонок</button>
+        <ul class="header__menu">
+          <li class="header__menu-item">
+            <router-link :to="{name: 'PriceList'}" class="header__menu-link" tag="a">Прайс-лист</router-link>
+          </li>
+          <li class="header__menu-item">
+            <router-link :to="{name: 'Dealers'}" class="header__menu-link" tag="a">Поставщикам</router-link>
+          </li>
+          <li class="header__menu-item">
+            <router-link :to="{name: 'Contacts'}" class="header__menu-link" tag="a">Контакты</router-link>
+          </li>
+        </ul>
+        <button
+          :class="['header__burger', {'header__burger--active': mobileMenuIsOpen}]"
+          aria-label="Открыть меню"
+          type="button"
+          @click="mobileMenuIsOpen = !mobileMenuIsOpen"
+        >
+          <span class="header__burger-line"></span>
+        </button>
       </div>
     </nav>
     <CallbackPopup :opened="openCallbackPopup" @close-popup="openCallbackPopup = false" />
+    <transition name="fade">
+      <div class="header__mobile-menu-box" v-if="mobileMenuIsOpen">
+        <ul class="header__mobile-menu">
+          <li class="header__mobile-menu-item">
+            <router-link
+              :to="{name: 'Home', hash: '#about'}"
+              class="header__mobile-menu-url"
+              tag="a"
+              @click.native="mobileMenuClickHandler('#about')"
+            >О компании</router-link>
+          </li>
+          <li class="header__mobile-menu-item">
+            <router-link
+              :to="{name: 'Products'}"
+              class="header__mobile-menu-url"
+              tag="a"
+              @click="mobileMenuClickHandler"
+            >Продукция</router-link>
+          </li>
+          <li class="header__mobile-menu-item">
+            <router-link
+              :to="{name: 'Logistics'}"
+              class="header__mobile-menu-url"
+              tag="a"
+              @click="mobileMenuClickHandler"
+            >Логистика</router-link>
+          </li>
+          <li class="header__mobile-menu-item">
+            <router-link
+              :to="{name: 'PriceList'}"
+              class="header__mobile-menu-url"
+              tag="a"
+              @click="mobileMenuClickHandler"
+            >Прайс-лист</router-link>
+          </li>
+          <li class="header__mobile-menu-item">
+            <router-link
+              :to="{name: 'Dealers'}"
+              class="header__mobile-menu-url"
+              tag="a"
+              @click="mobileMenuClickHandler"
+            >Поставщикам</router-link>
+          </li>
+          <li class="header__mobile-menu-item">
+            <router-link
+              :to="{name: 'Contacts'}"
+              class="header__mobile-menu-url"
+              tag="a"
+              @click="mobileMenuClickHandler"
+            >Контакты</router-link>
+          </li>
+        </ul>
+      </div>
+    </transition>
   </header>
 </template>
 
@@ -62,12 +120,20 @@
       return {
         phone: null,
         openCallbackPopup: false,
+        mobileMenuIsOpen: false,
       };
     },
     methods: {
       scrollFix(hashbang) {
         location.href = hashbang;
-      }
+      },
+      mobileMenuClickHandler(hashbang) {
+        this.mobileMenuIsOpen = false;
+
+        if (hashbang) {
+          this.scrollFix(hashbang);
+        }
+      },
     },
     created() {
       api.getCurrentPage('contacts', (response) => {
